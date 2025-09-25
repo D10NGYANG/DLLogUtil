b0c7e63c84977d087322061405dce76018e57e5c
D10NGYANG <D10NGYANG@163.com>
2025-09-25 14:21:24 +0800

    feat: 初步创建项目

---
+++ library/build.gradle.kts
@@ -0,0 +1,72 @@
+@file:OptIn(ExperimentalWasmDsl::class)
+
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
+    id("maven-publish")
+}
+
+group = "com.github.D10NGYANG"
+version = "0.1.0"
+
+kotlin {
+    jvmToolchain(8)
+    androidTarget {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_1_8
+        }
+        publishLibraryVariants("release")
+    }
+    jvm()
+    js(IR) {
+        browser()
+        binaries.library()
+    }
+    linuxX64()
+    linuxArm64()
+    macosX64()
+    macosArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            // 协程
+            implementation(libs.kotlinx.coroutines)
+        }
+    }
+}
+
+android {
+    namespace = "com.d10ng.log"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+val bds100MavenUsername: String by project
+val bds100MavenPassword: String by project
+
+afterEvaluate {
+    publishing {
+        repositories {
+            maven {
+                url = uri("/Users/d10ng/project/kotlin/maven-repo/repository")
+            }
+            maven {
+                credentials {
+                    username = bds100MavenUsername
+                    password = bds100MavenPassword
+                }
+                setUrl("https://nexus.bds100.com/repository/maven-releases/")
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+rootProject.name = "DLLogUtil-Project"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+include(":library")
+project(":library").name = "DLLogUtil"
+ 
